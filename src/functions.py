f9988e9e033347fe309a6cd5446b2d0c13a2755c
yuchild <yuchild@gmail.com>
2024-09-28 23:42:26 -0700

    Updated 2024-09-28

--- src/functions.py
+++ src/functions.py
@@ -79,8 +79,8 @@ def zscore(x, mu, stdev):
 # direction calculation:
 def direction(pctc, mean, stdev):
     
-    pct_pos = mean + 0.43073 / 2 * stdev
-    pct_neg = mean - 0.43073 / 2 * stdev
+    pct_pos = mean + 0.43073 / 1.31 * stdev
+    pct_neg = mean - 0.43073 / 1.31 * stdev
     if pctc >= pct_pos:
         return 1
     elif pctc <= pct_neg:
