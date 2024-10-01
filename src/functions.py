bb3908702433e8b38c15cfb56db3c409ae58c6d2
yuchild <yuchild@gmail.com>
2024-09-30 23:27:57 -0700

    Updated 2024-09-30

--- src/functions.py
+++ src/functions.py
@@ -79,8 +79,8 @@ def zscore(x, mu, stdev):
 # direction calculation:
 def direction(pctc, mean, stdev):
     
-    pct_pos = mean + 0.43073 / 2.13 * stdev
-    pct_neg = mean - 0.43073 / 2.13 * stdev
+    pct_pos = mean + 0.43073 / 1.67 * stdev
+    pct_neg = mean - 0.43073 / 1.67 * stdev
     if pctc >= pct_pos:
         return 1
     elif pctc <= pct_neg:
@@ -222,7 +222,7 @@ def model(symbol, interval):
     categorical_transformer = Pipeline(steps=[
         ('onehot', OneHotEncoder(handle_unknown='ignore',sparse_output=False))
     ])
-    
+   
     preprocessor = ColumnTransformer(
         transformers=[
             ('cat', categorical_transformer, categorical_features)
@@ -232,11 +232,11 @@ def model(symbol, interval):
     
     # Define your models
     models = {
-        'XGBoost': XGBClassifier(random_state=42, n_jobs=-1, learning_rate=0.1, max_depth=3, n_estimators=100),
-        'RandomForest': RandomForestClassifier(random_state=42, n_jobs=-1, max_depth=10, min_samples_split=2, n_estimators=200),
-        'GradientBoosting': GradientBoostingClassifier(random_state=42, learning_rate=0.5, max_depth=5, n_estimators=300),
+        'XGBoost': XGBClassifier(random_state=42, n_jobs=-1, learning_rate=0.1, max_depth=3, n_estimators=200),
+        'RandomForest': RandomForestClassifier(random_state=42, n_jobs=-1, max_depth=10, min_samples_split=13, n_estimators=10),
+        'GradientBoosting': GradientBoostingClassifier(random_state=42, learning_rate=0.01, max_depth=5, n_estimators=100, validation_fraction=0.25, n_iter_no_change=31),
         # 'LightGBM': LGBMClassifier(random_state=42,force_col_wise=True),
-        'KNN': KNeighborsClassifier(n_neighbors=7, p=1,weights='distance')
+        'KNN': KNeighborsClassifier(n_neighbors=5, p=1,weights='uniform')
     }
 
     
