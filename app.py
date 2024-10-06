eb55cf76f6ce7ec0d632361853a5965c5f4c19c3
yuchild <yuchild@gmail.com>
2024-10-06 13:26:19 -0700

    Updated 2024-10-06

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ import plotly.express as px
 from src.functions import dl_tf_pd, load
 
 # Container 1
-with st.container(): 
-    col1, col2 = st.columns([1,2])
-
-    col1.markdown("""
-    ### Next Time Stock Interval Time Frame Prediction App
+with st.container():
+    st.markdown(f"""
+    ##Next Stock Interval Time Frame Prediction App
+    
     """)
+    
+    
+# Container 2
+with st.container(): 
+    col1, col2, col3 = st.columns([1,2,3])
     # Text input
     symbol = col1.text_input("Enter your stock symbol in caps:", value='NVDA').upper()
 
@@ -20,24 +24,39 @@ with st.container():
 
     # Dropdown menu for intervals
     interval_options = ['5m', '15m','30m', '1h', '1d', '1wk']
-    selected_interval_option = col1.selectbox("Choose an interval option:", interval_options, index=0)
+    selected_interval_option = col2.selectbox("Choose an interval option:", interval_options, index=0)
 
     # Display the selected interval option
-    col1.write(f'You selected interval: {selected_interval_option}\n')
+    col2.write(f'You selected interval: {selected_interval_option}\n')
 
     # Dropdown menu for period option
     period_options = ['1mo', '3mo','1y', 'max']
-    selected_period_option = col1.selectbox("Choose an period option:", period_options, index=0)
+    selected_period_option = col3.selectbox("Choose an period option:", period_options, index=0)
 
     # Display the selected period option
-    col1.write(f'You selected period: {selected_period_option}')
+    col3.write(f'You selected period: {selected_period_option}')
 
+    
+# Container 3
+with st.container():
     # Run download, transform, and modeling
     time_stamp, summary_table = dl_tf_pd(symbol, 
                                          selected_interval_option, 
                                          selected_period_option, 
                                          skip_dl=False,
                                         )
+
+    # Display the current time
+    st.write(f"Current Time (EST): {time_stamp}\n")
+
+    # Display results summary table
+    st.table(summary_table.T)
+    
+    
+# Container 4
+with st.container():
+    col1, col2 = st.columns([1,2])
+    
     # Load stock dataframe
     stock_df = load(symbol, selected_interval_option).iloc[-200:]
 
@@ -45,14 +64,14 @@ with st.container():
     stock_df = stock_df.reset_index().rename(columns={'Datetime': 'date'})
     
     # display line chart of Close values
-    col2.write(f'{symbol} Price, last 200 intervals')    
+    col1.write(f'{symbol} Price, last 200 intervals')    
     fig = px.line(stock_df, x='date',  y='adj_close')
     fig.update_layout(
         xaxis_title='Date',
         yaxis_title='Price',
         xaxis_rangeslider_visible=True
     )
-    col2.plotly_chart(fig)
+    col1.plotly_chart(fig)
 
     # display line chart of Volume values
     col2.write(f'{symbol} Volume, last 200 intervals')
@@ -63,16 +82,6 @@ with st.container():
         xaxis_rangeslider_visible=True
     )
     col2.plotly_chart(fig)
-
-# Container 2 
-with st.container():
-    # Display the current time
-    st.write(f"Current Time (EST): {time_stamp}")
-
-    # Display results summary table
-    st.table(summary_table)
-
-
 
 
 
