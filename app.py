115cdcfc2a7397774f08b223d7a6435482e760e6
yuchild <yuchild@gmail.com>
2024-10-06 12:46:19 -0700

    Updated 2024-10-06

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ interval_options = ['5m', '15m','30m', '1h', '1d', '1wk']
 selected_interval_option = col1.selectbox("Choose an interval option:", interval_options, index=0)
 
 # Display the selected interval option
-col2.write(f'You selected interval: {selected_interval_option}\n')
+col1.write(f'You selected interval: {selected_interval_option}\n')
 
 # Dropdown menu for period option
 period_options = ['1mo', '3mo','1y', 'max']
 selected_period_option = col1.selectbox("Choose an period option:", period_options, index=0)
 
 # Display the selected period option
-col2.write(f'You selected period: {selected_period_option}')
+col1.write(f'You selected period: {selected_period_option}')
 
 # Run download, transform, and modeling
 time_stamp, summary_table = dl_tf_pd(symbol, 
